532b083b41759c0c2f9ec69afb59e4b3e8531c2a
James Rogers <hello@napacreativeco.com>
2021-04-08 14:08:02 -0700

    first update

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import "./layout.css"
 
+
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -26,24 +28,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+
+
+      <main>{children}</main>
+
+
+      <footer className="marquee">
+        <p style={{ lineHeight: '1.2' }}>MENDOCINO COUNTY MENDOCINO COUNTY MENDOCINO COUNTY MENDOCINO COUNTY MENDOCINO COUNTY MENDOCINO COUNTY MENDOCINO COUNTY </p>
+      </footer>
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,33 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import SlideShow from '../components/Slider.js'
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    
+    <SlideShow />
+
+    <div className="contact">
+      <div>
+        <h2>
+          MICHAEL FENTON<br />
+          MIKE@GRDNMNGMNT.COM
+        </h2>
+        <p style={{ margin: '10px 0px'}}>-------------------</p>
+        <h2>
+          SEAMUS GUEVARA<br />
+          SEAMUS@GRDNMNGMNT.COM
+        </h2>
+      </div>
+
+      <svg style={{ width: '100px' }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171.03 73.83"><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><circle cx="31.91" cy="36.91" r="31.91"/><path d="M134.12,73.83A36.92,36.92,0,1,1,171,36.91,37,37,0,0,1,134.12,73.83Zm0-63.83A26.92,26.92,0,1,0,161,36.91,26.95,26.95,0,0,0,134.12,10Z"/></g></g></svg>
+ 
+    </div>
+
   </Layout>
 )
 
